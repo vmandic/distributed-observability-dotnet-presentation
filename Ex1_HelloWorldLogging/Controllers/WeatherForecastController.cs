3a366fef0c7f7cb655587a200aac15a810656afa
Vedran Mandic <mandic.vedran@gmail.com>
2021-12-08 20:03:58 +0100

    serilog done

---
+++ Ex1_HelloWorldLogging/Controllers/WeatherForecastController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex1_HelloWorldLogging.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var name = "ATD-16";
+
+            // log info level
+            _logger.LogInformation("Hello logger!");
+
+            // log warn level
+            _logger.LogWarning("Hello logger warning!");
+
+            // log err level
+            _logger.LogError("Hello logger error!");
+
+            // log ftl level
+            _logger.LogCritical("Hello logger FATAL / CRITICAL!!!");
+
+            // log err level + excp
+            try
+            {
+                throw new Exception("Whoopsie daisy! Something went wrong...");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Hello logger error!");
+            }
+
+            // DO NOT DO THIS: log string interpolated
+            _logger.LogInformation($"Hello {name}! (string interpolated)", name);
+
+            // BUT DO THIS: log interpolated values to save parts as structured logging properties
+            _logger.LogInformation("Hello {author}! (log property interpolated)", name);
+
+            // use event ID to find quicker in windows event log, make sure you export to windows EventLog with EventLog log provider registration - windows only!
+            _logger.LogInformation(123, "Hello {author}!", name);
+
+            // create use custom event IDs with title
+            var e = new EventId(5555, "Some event");
+            _logger.LogWarning(e, "Whoops some warning occured at (UTC+0): {timestamp}", DateTime.UtcNow);
+
+            // enrich the log with contextual data
+            using var logscope = _logger.BeginScope("this is a scope with user '{user}' and role '{role}'", "user 123", "finance");
+            _logger.LogInformation("This is prop enriched message from log context");
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Ex1_HelloWorldLogging/Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Compact;
+using System;
+
+namespace Ex1_HelloWorldLogging
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // not so super cool as you create config... (use https://github.com/serilog/serilog-aspnetcore#two-stage-initialization) with CreateBootstrapLogger() instead
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // the global non-microsoft logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                //.WriteTo.Console(new RenderedCompactJsonFormatter())
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("App is starting...");
+                var appHost = CreateHostBuilder(args).Build();
+                appHost.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "You app did not manage to start");
+                throw;
+            }
+            finally
+            {
+                // make sure the log finishes before the app shuts down
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(opts => {
+
+                    // remove all default providers... recommended
+                    //opts.ClearProviders();
+                    //opts.AddConsole();
+                    //opts.AddEventLog(evOpts =>
+                    //{
+                    //    evOpts.LogName = "Ex1_HelloWorldLogging";
+                    //    evOpts.SourceName = "Ex1_HelloWorldLogging";
+                    //});
+                })
+                // will use the Log.Logger instance we set up in Main()
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+
+// start seq with docker locally
+// docker run --rm --name seq -d -e ACCEPT_EULA=Y -p 5341:5341 -p 8888:80 datalust/seq:latest
+
+// https://benfoster.io/blog/serilog-best-practices
